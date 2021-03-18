5cc3d7d7c4d063681a7b93ac2b280c6ef60b4efd
Sean Killeen <SeanKilleen@gmail.com>
2021-03-18 15:23:23 -0400

    add repo information for deployment

--- infrastructure/functions-app.tf
+++ infrastructure/functions-app.tf
@@ -43,4 +43,10 @@ resource "azurerm_function_app" "example" {
   app_service_plan_id        = azurerm_app_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  source_control {
+    repo_url           = "https://github.com/SeanKilleen/terraform-azure-functions"
+    branch             = "main"
+    manual_integration = false
+    rollback_enabled   = true
+  }
 }
