c988f68618c21ef1619f05560a9e66e8bbb22840
Sean Killeen <SeanKilleen@gmail.com>
2021-03-18 15:00:02 -0400

    basic test infrastructure

---
+++ infrastructure/functions-app.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "example" {
+  name     = "azure-functions-seandemo"
+  location = "East US 2"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "functionsappseandemo"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-seandemo-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "azure-functions-seandemo"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}
